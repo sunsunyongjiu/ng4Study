382cddb389b518a7507e9848d2346dd22b38fd3f
sunshiyi <shiyi.sun@htwins.com.cn>
2018-03-14 13:40:36 +0800

    feat:初始化

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'detail', component: DetailComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
